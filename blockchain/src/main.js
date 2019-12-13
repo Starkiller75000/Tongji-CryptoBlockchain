b3e58c4ec0644958c51d12b91db09ed85e903edf
Benoit Pingris <benoit.pingris@epitech.eu>
2019-12-13 17:47:48 +0800

    added basic things

--- blockchain/src/main.js
+++ blockchain/src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
+import store from './store'
+import router from './router'
+
 import App from './App.vue'
+import vuetify from './plugins/vuetify';
+import '@babel/polyfill'
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router,
+  vuetify,
+  store
 }).$mount('#app')
